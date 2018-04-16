27c548c0304fdd73f3979c517e9af7f77522a227
cremzoo <cremzoo@yahoo.com>
2018-04-16 16:54:06 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+$("h1").html("<b>Juan Ramón Jiménez</b>");
+$("h2").html("<i>Spanish Poet</i>");
+$("h3").html("<i>(24 December 1881 – 29 May 1958)</i>");
+$("h4").html("<b>El Viaje Definitivo </b>");
+$("h5").html("<b>The Definitive Journey</b>");
+$("h6").html("<i>Translation by Lila Robertson, 2002</i>");
