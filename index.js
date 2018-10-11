09e6924f13f49a39f33b7feca56d7f305da854fb
Cremona Matei <cremzoo@yahoo.com>
2018-10-11 12:21:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-$("h1").html("<b>Juan Ramón Jiménez</b>");
-$("h2").html("<i>Spanish Poet</i>");
-$("h3").html("<i>(24 December 1881 – 29 May 1958)</i>");
-$("h4").html("<b>El Viaje Definitivo </b>");
-$("h5").html("<b>The Definitive Journey</b>");
-$("h6").html("<i>Translation by Lila Robertson, 2002</i>");
+$(document).ready(function() {
+  $.backstretch(
+    "https://res.cloudinary.com/cremz/image/upload/v1525849215/grunge texture1.png"
+  );
+});
